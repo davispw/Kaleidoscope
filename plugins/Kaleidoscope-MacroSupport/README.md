4463b17bfea5d4281a861314b236f9b154bcd4ae
Peter Davis <davispw@gmail.com>
2025-04-18 17:30:13 -0700

    Refactor Macro playback function to reduce duplication.

--- plugins/Kaleidoscope-MacroSupport/README.md
+++ plugins/Kaleidoscope-MacroSupport/README.md
@@ -1,10 +1,10 @@
 # MacroSupport
 
-This plugin provides the supplemental key array used by the Macros and DynamicMacros plugins, and is necessary for the proper functioning of those plugins.
+This plugin provides the supplemental key array used by the Macros, DynamicMacros, and EphemeralMacros plugins, and is necessary for the proper functioning of those plugins.
 
 ## Using the plugin
 
-Any firmware sketch that uses either Macros or DynamicMacros automatically includes this plugin, so there's no need to add it explicitly.  If your sketch doesn't require either type of Macros key, however, you can still make use of the MacroSupport plugin's helper methods (`tap()`, `press()`, _et al_).  In that case, you should include the MacroSupport header file, and include it in `KALEIDOSCOPE_INIT_PLUGINS()`:
+Any firmware sketch that uses either Macros, DynamicMacros, or EphemeralMacros automatically includes this plugin, so there's no need to add it explicitly.  If your sketch doesn't require either type of Macros key, however, you can still make use of the MacroSupport plugin's helper methods (`tap()`, `press()`, _et al_).  In that case, you should include the MacroSupport header file, and include it in `KALEIDOSCOPE_INIT_PLUGINS()`:
 
 ```c++
 #include <Kaleidoscope.h>
@@ -42,10 +42,10 @@ The plugin provides a `MacroSupport` object, which contains a supplemental array
 > supplemental keys array (see above) and sends a release event for each key
 > stored there.
 
+It is not necessary to use any Macros plugins to make use of MacroSupport.  When using it with custom code, however, please remember that the supplemental active keys array it provides will be shared by all clients (e.g. Macros, user-defined Leader or TapDance functions), so if you want more than one of those clients to be active simultaneously, be aware that calls to `MacroSupport.clear()` will affect all of them, not just the caller.
+
 ### `.tap(key)`
 
 > Sends an immediate press and release event for `key` with no delay, using an
 > invalid key address.  This method doesn't actually use the supplemental keys
 > array, but is provided here for convenience and simplicity.
-
-It is not necessary to use either the Macros (or DynamicMacros) to make use of MacroSupport.  When using it with custom code, however, please remember that the supplemental active keys array it provides will be shared by all clients (e.g. Macros, user-defined Leader or TapDance functions), so if you want more than one of those clients to be active simultaneously, be aware that calles to `MacroSupport.clear()` will affect all of them, not just the caller.
--- plugins/Kaleidoscope-MacroSupport/src/kaleidoscope/plugin/MacroSupport.h
+++ plugins/Kaleidoscope-MacroSupport/src/kaleidoscope/plugin/MacroSupport.h
@@ -27,6 +27,11 @@
 #include "kaleidoscope/key_defs.h"              // for Key
 #include "kaleidoscope/plugin.h"                // for Plugin
 
+// This is a special exception to the rule of only including a plugin's
+// top-level header file, because MacroSupport doesn't depend on the Macros
+// plugin itself; it's just using the same macro step definitions.
+#include "kaleidoscope/plugin/Macros/MacroSteps.h"  // for MACRO_ACTION_END, MACRO_ACTION_STEP_...
+
 // =============================================================================
 // The number of simultaneously-active `Key` values that a macro can have
 // running during a call to `Macros.play()`. I don't know if it's actually
@@ -67,6 +72,139 @@ class MacroSupport : public Plugin {
   /// Generates two new `KeyEvent` objects, one each to press and release the
   /// specified `key`, passing both in sequence to `Runtime.handleKeyEvent()`.
   void tap(Key key) const;
+
+  /// Play a macro.
+  ///
+  /// This function reads a sequence of macro steps from memory and
+  /// presses/releases/taps keys as instructed.
+  ///
+  /// This is intended to be called from other plugins.
+  /// Macros, DynamicMacros, and EphemeralMacros are core plugins that
+  /// use this method and implement the `Accessor` template type.
+  ///
+  /// The Accessor must provide the following fields and methods:
+  ///
+  /// * `uint8_t readByte()`. This method reads one byte and advances.
+  ///   The first returned value should be a `MacroActionStepType`.
+  ///   Depending on the step type, this is followed by zero or more bytes
+  ///   containing key flag(s), key code(s), or a delay time (in
+  ///   milliseconds).
+  ///
+  /// * `static constexpr bool had_boundary`. If true, then it must also
+  ///   provide an `bool isEnd()` method. Otherwise the end of the macro
+  ///   is indicated by a MACRO_STEP_END value (for the main loop), or a
+  ///   Key_NoKey value (for a MACRO_ACTION_STEP_TAP_SEQUENCE loop).
+  template<typename Accessor>
+  void play(Accessor &accessor, uint16_t default_interval = 0)
+  // Template function body must be defined in header:
+  {
+    macro_t macro = MACRO_ACTION_END;
+    Key key;
+    uint16_t interval = default_interval;
+
+    auto setKeyAndAction = [&key, &macro, &accessor, this]() {
+      uint8_t flags   = (macro == MACRO_ACTION_STEP_KEYCODEDOWN ||
+                       macro == MACRO_ACTION_STEP_KEYCODEUP ||
+                       macro == MACRO_ACTION_STEP_TAPCODE)
+                          ? 0
+                          : accessor.readByte();
+      uint8_t keycode = accessor.readByte();
+
+      key.setFlags(flags);
+      key.setKeyCode(keycode);
+
+      switch (macro) {
+      case MACRO_ACTION_STEP_KEYCODEDOWN:
+      case MACRO_ACTION_STEP_KEYDOWN:
+        press(key);
+        break;
+      case MACRO_ACTION_STEP_KEYCODEUP:
+      case MACRO_ACTION_STEP_KEYUP:
+        release(key);
+        break;
+      case MACRO_ACTION_STEP_TAP:
+      case MACRO_ACTION_STEP_TAPCODE:
+        tap(key);
+        break;
+      default:
+        break;
+      }
+    };
+
+    // Main playback loop - always loops until explicit break/return
+    while (true) {
+      // Boundary check *before* reading,
+      // only for accessors that need it (SRAM/EEPROM).
+      if constexpr (Accessor::has_boundary)
+        if (accessor.isEnd())
+          break;  // Reached end of buffer/storage
+
+      macro = accessor.readByte();
+
+      // Check for explicit end marker *after* reading (primary check for PGM)
+      if (macro == MACRO_ACTION_END) {
+        break;  // Normal termination
+      }
+
+      switch (macro) {
+      case MACRO_ACTION_STEP_EXPLICIT_REPORT:
+      case MACRO_ACTION_STEP_IMPLICIT_REPORT:
+      case MACRO_ACTION_STEP_SEND_REPORT:
+        break;
+
+      case MACRO_ACTION_STEP_INTERVAL:
+        interval = accessor.readByte();
+        break;
+
+      case MACRO_ACTION_STEP_WAIT: {
+        uint8_t wait = accessor.readByte();
+        // NOTE: If reading 'wait' consumes the last byte for a boundary-checking
+        // accessor, the loop will terminate correctly on the next iteration's check.
+        delay(wait);
+        break;
+      }
+
+      case MACRO_ACTION_STEP_KEYDOWN:
+      case MACRO_ACTION_STEP_KEYUP:
+      case MACRO_ACTION_STEP_TAP:
+      case MACRO_ACTION_STEP_KEYCODEDOWN:
+      case MACRO_ACTION_STEP_KEYCODEUP:
+      case MACRO_ACTION_STEP_TAPCODE:
+        setKeyAndAction();
+        // NOTE: Similar to WAIT, if reading key data consumes the last byte,
+        // the loop terminates on the next iteration's boundary check (if applicable).
+        break;
+
+      case MACRO_ACTION_STEP_TAP_SEQUENCE:
+      case MACRO_ACTION_STEP_TAP_CODE_SEQUENCE: {
+        bool isKeycodeSequence = macro == MACRO_ACTION_STEP_TAP_CODE_SEQUENCE;
+        while (true) {
+          if constexpr (Accessor::has_boundary)
+            if (accessor.isEnd())
+              break;  // Reached end of buffer/storage during sequence
+
+          uint8_t flags   = isKeycodeSequence ? 0 : accessor.readByte();
+          uint8_t keycode = accessor.readByte();
+
+          key.setFlags(flags);
+          key.setKeyCode(keycode);
+
+          // Check for end-of-sequence marker
+          if (key == Key_NoKey) break;
+
+          tap(key);
+          delay(interval);
+        }
+      }
+
+      MACRO_ACTION_END:
+      default:  // Unknown action treated like end
+        return;
+      }
+
+      delay(interval);
+    }
+  }
 
   // ---------------------------------------------------------------------------
   // Event handlers
