de1bc422461caaccfc16a47f77d6a803612547ac
Peter Davis <davispw@gmail.com>
2025-04-19 10:43:43 -0700

    Add placeholders for headers moved in prev commit with deprecation warning.

---
+++ plugins/Kaleidoscope-Macros/src/kaleidoscope/plugin/Macros/MacroKeyDefs.h
@@ -0,0 +1,5 @@
+/* DEPRECATED: header moved to 'kaleidoscope/plugin/MacroKeyDefs.h'. */
+
+#pragma once
+
+#include "kaleidoscope/plugin/MacroKeyDefs.h"  // IWYU pragma: export
---
+++ plugins/Kaleidoscope-Macros/src/kaleidoscope/plugin/Macros/MacroSteps.h
@@ -0,0 +1,5 @@
+/* DEPRECATED: header moved to 'kaleidoscope/plugin/MacroSteps.h'. */
+
+#pragma once
+
+#include "kaleidoscope/plugin/MacroSteps.h"  // IWYU pragma: export
